0e0a8caabc43abbfb8fc4cd68c55058b6666c1a9
Grigoriy Skobelev <gskobelevff@gmail.com>
2022-04-05 16:54:42 +0300

    Add test

---
+++ src/test/java/com/skobelev/MainTest.java
@@ -0,0 +1,22 @@
+package com.skobelev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    void findMax() {
+        Main service = new Main();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+
+}
